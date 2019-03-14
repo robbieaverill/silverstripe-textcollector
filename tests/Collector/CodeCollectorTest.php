1b47370c18c24f368b4eba873ebbd95d6c390eb2
Robbie Averill <robbie@silverstripe.com>
2019-03-15 10:50:33 +1300

    Add test for concatenating integers

--- tests/Collector/CodeCollectorTest.php
+++ tests/Collector/CodeCollectorTest.php
@@ -78,5 +78,6 @@ class CodeCollectorTest extends SapphireTest
 
         $this->assertSame('Line 1 and Line \'2\' and Line "3"', $result['Test.CONCATENATED']);
         $this->assertSame('Line 1 Line 2 Line 3 Line 4 Line 5 Line 6', $result['Test.MOARCONCATENATED']);
+        $this->assertSame('I would like 3 lunches please', $result['Test.INTCONCAT']);
     }
 }
